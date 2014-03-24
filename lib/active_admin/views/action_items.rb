36b28a5cc520ce79967783162ef0b8284ce387c4
Zidni Mubarock <zidmubarock@gmail.com>
2014-03-24 19:47:38 +0700

    pagination and fix empty action_item

--- lib/active_admin/views/action_items.rb
+++ lib/active_admin/views/action_items.rb
@@ -5,9 +5,10 @@ module ActiveAdmin
 
       def build(action_items)
         action_items.each do |action_item|
+          ie = instance_exec(&action_item.block)
           span class: "action_item btn btn-sm btn-default" do
-            instance_exec(&action_item.block)
-          end
+            ie
+          end unless ie.blank?
         end
       end
 
